cf992276e9704ce4a2a412694c4a86238e053ff2
Thinh Nguyen Kim Hung <thinh.nguyen@orientsoftware.com>
2023-04-04 09:44:07 +0700

    update model

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 from fastapi import FastAPI, Request, UploadFile
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
 
 import os
+from datetime import datetime
+
 import whisper
 import torch
-from datetime import datetime
+from config import Config
+from model import WhisperModelModule
 
 from utils import hf_to_whisper_states
 
 from enum import Enum
+
 
 app = FastAPI()
 
@@ -26,26 +30,50 @@ app.add_middleware(
 # Check device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# Load HF Model
-hf_state_dict = torch.load('small_vi_02.bin', map_location=torch.device('cpu'))    # *.bin file
 
-# Rename layers
+# Load model small
+## Load HF Model
+hf_state_dict = torch.load('small_vi_02.bin', map_location=torch.device(device))    # *.bin file
+
+## Rename layers
 for key in list(hf_state_dict.keys())[:]:
     new_key = hf_to_whisper_states(key)
     hf_state_dict[new_key] = hf_state_dict.pop(key)
 
-# Init Whisper Model and replace model weights
+## Init Whisper Model and replace model weights
 model = whisper.load_model('small')
 model.load_state_dict(hf_state_dict)
+
+
+# Load model base
+config = Config()
+config.checkpoint_path = 'checkpoint-epoch=0014.ckpt'
+
+module = WhisperModelModule(config)
+try:
+    state_dict = torch.load(config.checkpoint_path, map_location=torch.device(device))
+    state_dict = state_dict["state_dict"]
+    module.load_state_dict(state_dict)
+    # print(f"load checkpoint successfully from {config.checkpoint_path}")
+except Exception as e:
+    print(e)
+    print(f"load checkpoint failt using origin weigth of {config.model_name} model")
+base_model = module.model
+base_model.to(device)
 
 
 class language(str, Enum):
     en = 'English'
     vi = 'Vietnamese'
 
+class version(str, Enum):
+    small = 'small'
+    base = 'base'
+
+
 
 @app.post("/Whisper/")
-async def speech_to_text(file: UploadFile, language: language):
+def speech_to_text(file: UploadFile, language: language, version: version):
     audio_path = file.filename
 
     audio = whisper.load_audio(audio_path)
@@ -56,7 +84,10 @@ async def speech_to_text(file: UploadFile, language: language):
     options = whisper.DecodingOptions(
     language=language, without_timestamps=True, fp16=torch.cuda.is_available()
 )
+    if version == 'small':
+        result = whisper.decode(model, mel, options)
+    elif version == 'base':
+        result = base_model.decode(mel, options)
     
-    result = whisper.decode(model, mel, options)
-
     return {"transcribe": result.text}
+
